7c325311015a65fc949d33636c56501d0ec046db
aliceclimas <alicecristinalima31@gmail.com>
2025-06-16 11:36:16 -0300

    aula 65 finalizada

---
+++ src/aula65/Teste.java
@@ -0,0 +1,22 @@
+package aula65;
+
+@InformacaoAula(
+    autor = "Loaine",
+    aulaNumero = 65,
+    blog = "loaine.com"
+)
+public class Teste {
+
+    @InformacaoAula(
+            autor = "Loaine",
+            aulaNumero = 65,
+            blog = "loiane.com"
+        )
+    public static void main(String[] args) {
+
+        
+        
+        
+    }
+
+}
